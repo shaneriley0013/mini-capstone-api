f3a3b3772061ef9650eb95856caa75a9e7041579
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-29 14:13:04 -0500

    add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+
+
+  def show
+    @product = Product.first   
+    render template: "products/show"
+  end
+
+  def all
+    @products = Product.all
+    render template: "products/index"
+  end
+  
+  
+  
+  
+  
+  
+  
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new( name: "Burberry Shirt", price: 350, image_url: "https://assets.burberry.com/is/image/Burberryltd/85754AD2-B452-4FE9-A7CA-BE0FFD8D6B0F?$BBY_V2_SL_3x4$&wid=1278&hei=1700", description: "Large Nova Check" )
+
+product.save
