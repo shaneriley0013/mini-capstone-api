3572feae8f2f7c76f26768c6f59a2e6d1af85ae0
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-02 14:05:27 -0500

    delete spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
-
-
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 class ProductsController < ApplicationController
 
 
