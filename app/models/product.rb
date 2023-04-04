ee352ddeedf93d19c32565bb23f7fcda2e9c6080
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-04 14:34:40 -0500

    update with change descrip, add functions

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ApplicationRecord
 
-  def tax
-    tax = price * 1.10
-    return tax
+def is_discounted?
+  
+  if price <= 10
+    return true
+  else
+    return false
   end
 
-
-
-
+  def tax
+    tax = price * 0.09
+  end
+  
+  def total
+    price + tax
+  end
 
 end
--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -2,6 +2,7 @@ json.id product.id
 json.name product.name
 json.price product.price
 json.tax product.tax
+json.total product.total
 json.image_url product.image_url
 json.description product.description
 json.created_at product.created_at
