01ab24d567ec6844daf41d61ee75c9104d452eaa
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-04 19:30:39 -0500

    make product.rb code work, all aspects working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,18 @@
 class Product < ApplicationRecord
-
-def is_discounted?
-  if price <= 10
-    return true
-  else
-    return false
-end
+  def is_discounted?
+    #!(price >= 10)  #FANCY WAY
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end
 
   def tax
-    tax = price * 0.09
+    price * 0.09
   end
-  
+
   def total
     price + tax
   end
-
 end
